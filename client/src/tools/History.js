426bd33f1bbb3a8ef4cb1b9c70b3109651fc8932
Vsevolod Sashin <vsevolodshashin@gmail.com>
2025-01-20 22:48:47 +0300

    Rewrite Undo/Redo system. History sync

--- client/src/tools/History.js
+++ client/src/tools/History.js
@@ -19,11 +19,12 @@ class History {
         this.canvas = canvasState.canvas
         this.ctx = canvasState.ctx
 
-        const offscreenCanvas = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-        const ctx = offscreenCanvas.getContext('2d');
+        const canvas = document.createElement('canvas');
+        canvas.width = this.canvas.width;
+        canvas.height = this.canvas.height;
+        const ctx = canvas.getContext('2d');
         ctx.drawImage(this.canvas, 0, 0);
-        this.undoList.push(offscreenCanvas);
-
+        this.undoList.push(canvas);
     }
 
     pushToRedo() {
@@ -33,22 +34,36 @@ class History {
 
         this.canvas = canvasState.canvas
         this.ctx = canvasState.ctx
-        
-        const offscreenCanvas = new OffscreenCanvas(this.canvas.width, this.canvas.height);
-        const ctx = offscreenCanvas.getContext('2d');
+
+        const canvas = document.createElement('canvas');
+        canvas.width = this.canvas.width;
+        canvas.height = this.canvas.height;
+        const ctx = canvas.getContext('2d');
         ctx.drawImage(this.canvas, 0, 0);
 
-        this.redoList.push(offscreenCanvas)
+        this.redoList.push(canvas)
     }
 
+    
 
     Undo() {
         if (this.undoList.length == 0) { return }
 
         this.pushToRedo()
         this.ptReplase = this.undoList.pop()
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.drawImage(this.ptReplase, 0, 0)
+
+
+        canvasState.socket.send(
+              JSON.stringify({
+                id: canvasState.sessionid,
+                method: "draw",
+                figure: {
+                  type: "undo",
+                  canvasState: this.ptReplase.toDataURL(),
+                },
+              }
+            )
+        );
     }
 
     Redo() {
@@ -56,8 +71,17 @@ class History {
 
         this.pushToUndo()
         this.ptReplase = this.redoList.pop()
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.drawImage(this.ptReplase, 0, 0)
+        canvasState.socket.send(
+            JSON.stringify({
+              id: canvasState.sessionid,
+              method: "draw",
+              figure: {
+                type: "undo",
+                canvasState: this.ptReplase.toDataURL(),
+              },
+            }
+        )
+      );
     }
 }
 
